15973bd326d4f2a32d8df29131ebeb657fc7fb4b
linhdhng <153128184+linhdhng@users.noreply.github.com>
2024-02-14 12:52:48 -0600

    s.js

--- 308A/s.js
+++ 308A/s.js
@@ -1,10 +1,18 @@
 // Function to fetch data from the API
 async function fetchData() {
     try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
         const response = await fetch('https://pokeapi.co/api/v2/pokemon/espeon');
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
         const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
         return data;
     } catch (error) {
-        console.error('Error fetching data:', error);
+       console.error(error)
     }
 }
